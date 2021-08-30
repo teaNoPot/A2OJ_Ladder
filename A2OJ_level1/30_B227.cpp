da1bd3e6f4887723a882b34f44b297d188f0f841
Abdel <serghiinc@gmail.com>
2021-08-30 21:46:47 +0200

    B227 Solved
    Note : compare function uses a map instead of a double loop which had an order of complexity of O(n*m) - usage of uint64_t instead of just int to prevent signed int overflow

--- A2OJ_level1/30_B227.cpp
+++ A2OJ_level1/30_B227.cpp
@@ -2,13 +2,49 @@
  * B. Effective approach
  */
 
- #include <bits/stdc++.h>
- #define _ ios_base::sync_with_stdio(0);cin.tie(NULL);
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(NULL);
 
- using namespace std;
+using namespace std;
 
- int main() {
-   
+pair<uint64_t, uint64_t> compare(int n, vector<uint64_t> w, int m, vector<uint64_t> q) {
 
-     return 0;
- }
+    unordered_map<uint64_t, uint64_t> index;
+    for(int i=0; i<n; i++) 
+        index[w[i]] = i+1;
+
+    uint64_t left = 0, right = 0;
+    for(int i=0; i<m; i++) {
+        left += index[q[i]];
+        right += n - index[q[i]] + 1;
+    }
+
+    return make_pair(left, right);
+}
+
+
+int main() {
+
+    // INPUT
+    long n,m;
+    cin >> n;
+    vector<uint64_t> a(n);
+    for(int i=0; i<n; i++) { // O(n)
+        cin >> a[i];
+    }
+
+    cin >> m;
+    vector<uint64_t> b(m);
+    for(int i=0; i<m; i++) { // O(m) 
+        cin >> b[i];
+    }
+
+    // O(n*m)
+    pair<uint64_t, uint64_t> res = compare(n,a,m,b);
+
+
+    // OUTPUT
+    cout << res.first << " " << res.second << endl;
+
+    return 0;
+}
