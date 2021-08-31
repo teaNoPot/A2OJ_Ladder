2882405696714297c9d751bc3673d326025f0c76
Abdel <serghiinc@gmail.com>
2021-08-31 10:09:02 +0200

    271A Solution
    Pretty straight forward, it will not land on Dima if the SUM of all fingers shows is a MULTIPLE of the number of Dima and her friends.

--- A2OJ_level1/31_272A.cpp
+++ A2OJ_level1/31_272A.cpp
@@ -10,9 +10,21 @@ using namespace std;
 // Time complexity , Space complexity
 int main() {
 
-    // INPUT
-
-    // OUTPUT
+    int n;
+    cin >> n;
+    int sum = 0, finger;
+    for (int i=0; i<n; i++) {
+        cin >> finger;
+        sum+=finger;
+    }
+    
+    int solution;
+    for (int i=1; i<6; i++) {
+        if((sum+i)%(n+1) == 0)  {
+            solution = i;
+            break;
+        }
+    }
 
     return 0;
 }
