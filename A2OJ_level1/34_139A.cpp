f3ea6fc8cdf250499fd05a3a0f61fd4141b93cc2
Abdel <serghiinc@gmail.com>
2021-09-01 22:31:14 +0200

    Working solution to A139

--- A2OJ_level1/34_139A.cpp
+++ A2OJ_level1/34_139A.cpp
@@ -10,7 +10,26 @@ using namespace std;
 // Time complexity , Space complexity
 int main() {
 
+    int N;
+    cin >> N;
+
+    vector<int> a(7);
     
+    for(int i=0; i<7; i++) {
+        cin >> a[i];
+    }
+    int target = -1;
+    while(N>0) {
+        for (int i=0; i<7; i++) {
+            N -= a[i];
+            if(N <= 0) {
+                target = i;
+                break;
+            }
+        }
+    }
+    
+    cout << target+1 << endl;
 
     return 0;
 }
