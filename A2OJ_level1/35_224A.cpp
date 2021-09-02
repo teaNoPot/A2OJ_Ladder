dbba6eb4ebbbf264cde9294d1da93e0254cbe220
Abdel <serghiinc@gmail.com>
2021-09-02 17:05:52 +0200

    Solution to 224A
    Using simple geometry to determine the value of edges based off three areas sharing a common vertex

---
+++ A2OJ_level1/35_224A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(NULL);
+
+using namespace std;
+
+// Time complexity , Space complexity
+int main() {
+
+    vector<int> e(3);
+    cin >> e[0] >> e[1] >> e[2];
+
+    int a,b,c;
+
+    a = round(sqrt((e[0]*e[2])/e[1]));
+    b = round(sqrt((e[0]*e[1])/e[2]));
+    c = round(sqrt((e[1]*e[2])/e[0]));
+
+    cout << a*4+b*4+c*4 << endl;
+
+
+    return 0;
+}
