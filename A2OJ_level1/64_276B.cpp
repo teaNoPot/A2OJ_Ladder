a2a40a2c85398f97f504b7e0d17cc0941c3de399
Abdel <serghiinc@gmail.com>
2021-09-10 16:22:59 +0200

    Working solution
    Idea is to find the number of odd occurences, and using these letters to figure out who would win (in case there is 0,1 or more (even or odd) )
    - case 0 : First player instantly wins 
    - case 1 : First player wins, he can make a palindrome out of even letters and insert the rest in themiddle ( e.g abbccca => abcccba (second erases one odd, and first wins) )
    - case k > 1 : a game of moves (either increase k or dec by 1, if it's even (=2 e.g) both moves leads first player to lose, if it's odd the same is happening for the second player)

--- A2OJ_level1/64_276B.cpp
+++ A2OJ_level1/64_276B.cpp
@@ -2,36 +2,29 @@
 #define _ ios_base::sync_with_stdio(0);cin.tie(NULL);
 
 using namespace std;
+#define SIZE 26
 
 int main()
 {
     string a;
     cin >> a;
 
-    int n = a.length(), t = 0;
-    bool pal;
+    int n = a.length(), k=0;
 
-    while(t < 1001)
-    {
-        int d = -1;
-        int n = a.length();
-        pal = true;
-        for(int i=0; i<n/2; ++i)
-        {
-            if(a[i]!=a[n-i-1])
-            {
-                pal = false;
-                a.erase(a.begin()+i);
-                t++;
-                break;
-            }
-        }
+    int freq[SIZE];
+    memset(freq, 0, sizeof(freq));
+    for (int i = 0; i < n; i++)
+        freq[a[i] - 'a']++;
 
-        if(pal == true) break;
+    for (int i = 0; i < SIZE; i++) {
+        if(freq[i]%2==1)
+            k++;
     }
 
-    if(t%2==0) cout << "First" << endl;
-    if(t%2!=0) cout << "Second" << endl;
+    if(k==0)cout << "First" << endl;
+    else if (k==1) cout << "First" << endl;
+    else if (k%2==0) cout << "Second" << endl;
+    else cout << "First" << endl;
 
     return 0;
 }
